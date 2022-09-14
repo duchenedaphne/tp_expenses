cd7761382b17eae5b823de7f70cd676bed12e9e2
duchenedaphne <duchenedaphne@outlook.com>
2022-09-14 18:51:55 +0200

    ne fonctionne pas

--- app/src/main/java/com/doranco/tp_expenses/MainActivity.kt
+++ app/src/main/java/com/doranco/tp_expenses/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.doranco.tp_expenses
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -13,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.doranco.tp_expenses.ui.theme.Tp_expensesTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
