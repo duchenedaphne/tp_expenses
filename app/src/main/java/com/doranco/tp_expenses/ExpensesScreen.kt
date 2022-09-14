2bbd841e7c129744fc940a45a8e39c69809f913a
DavidMalinen <davidmalinen@outlook.com>
2022-09-14 15:22:01 +0200

    initial david commit

--- app/src/main/java/com/doranco/tp_expenses/ExpensesScreen.kt
+++ app/src/main/java/com/doranco/tp_expenses/ExpensesScreen.kt
@@ -1,5 +1,8 @@
 package com.doranco.tp_expenses
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.view.Display
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,54 +11,108 @@ import androidx.compose.foundation.magnifier
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.doranco.tp_expenses.ExpensesViewModel
+import com.doranco.tp_expenses.R
 
 @Preview(showBackground = true)
 @Composable
 fun ExpensesScreen() {
     val viewModel: ExpensesViewModel = viewModel()
-    LazyColumn(contentPadding = PaddingValues(vertical = 8.dp, horizontal = 8.dp)) {
+
+    FilterDetails()
+
+    LazyColumn(
+        contentPadding = PaddingValues(vertical = 60.dp, horizontal = 8.dp), modifier = Modifier
+    ) {
         items(viewModel.state.value) { expense ->
-            ExpenseItem(expense),
-            filter ->
-            FilterItem(filter)
+            ExpenseItem(expense)
         }
     }
 }
 
 @Composable
-fun FilterItem(item: Filter) {
-    Card(elevation = 4.dp, modifier = Modifier.padding(8.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
-            FilterDetails(item.time, item.currency, item.total, Modifier.weight(0.85f))
+fun FilterDetails() {
+    val text_time = remember {
+        mutableStateOf("Last 7 Days")
+    }
+    val text_price = remember {
+        mutableStateOf("67.16")
+    }
+
+    Card(
+        elevation = 4.dp, modifier = Modifier.padding(16.dp).width(360.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = {
+                    text_time.value = "Last 14 Days"
+                    println("FilterDetails: bouton cliqué")
+                    Log.d(TAG, "FilterDetails: cliqué")
+                },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(id = R.color.purple_200),
+                    contentColor = colorResource(id = R.color.white),
+                ),
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+            ) {
+                Text(text = text_time.value, style = MaterialTheme.typography.body2)
+            }
+
+            Button(
+                onClick = {  },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(id = R.color.purple_200),
+                    contentColor = colorResource(id = R.color.white)
+                ),
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+            ) {
+                Text(text = "$", style = MaterialTheme.typography.h6)
+            }
+
+            Button(
+                onClick = {
+                    text_price.value = "00.00"
+                },
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(id = R.color.purple_200),
+                    contentColor = colorResource(id = R.color.white)
+                ),
+                shape = MaterialTheme.shapes.large,
+                modifier = Modifier
+            ) {
+                Text(text = text_price.value, style = MaterialTheme.typography.h6)
+            }
         }
     }
-}
-
-@Composable
-fun FilterDetails(time: String, currency: String, total: Double, modifier: Modifier) {
-    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
-        Column(modifier = modifier) {
-            Text(text = time, style = MaterialTheme.typography.body2)
-        }
-        Column(modifier = modifier) {
-            Text(text = currency, style = MaterialTheme.typography.body2)
-        }
-        Column(modifier = modifier, horizontalAlignment = Alignment.End) {
-            Text(text = total.toString(), style = MaterialTheme.typography.h6)
-        }
 }
 
 @Composable
 fun ExpenseItem(item: Expense) {
-    Card(elevation = 4.dp, modifier = Modifier.padding(8.dp)) {
-        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+    Card(
+        elevation = 4.dp, modifier = Modifier.padding(8.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)
+        ) {
             ExpenseDetails(item.title, item.date, item.price, Modifier.weight(0.85f))
         }
     }
@@ -63,36 +120,36 @@ fun ExpenseItem(item: Expense) {
 
 @Composable
 fun ExpenseDetails(title: String, date: String, price: Double, modifier: Modifier) {
-    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween) {
-        Column(modifier = modifier) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = modifier
+        ) {
             Text(text = title, style = MaterialTheme.typography.h6)
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+
+            CompositionLocalProvider(
+                LocalContentAlpha provides ContentAlpha.medium
+            ) {
                 Text(text = date, style = MaterialTheme.typography.body2)
             }
         }
-        Column(modifier = modifier, horizontalAlignment = Alignment.End) {
-            Text(text = price.toString(), style = MaterialTheme.typography.button,
-                modifier = modifier.padding(8.dp, 1.dp).background(color = Color.Cyan, shape = RectangleShape))
+
+        Column(
+            modifier = modifier, horizontalAlignment = Alignment.End
+        ) {
+            Button(
+                onClick = {  },
+                modifier = Modifier,
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(id = R.color.teal_200),
+                    contentColor = colorResource(id = R.color.white)
+                ),
+                shape = MaterialTheme.shapes.large
+            ) {
+                Text(text = price.toString(), style = MaterialTheme.typography.h6)
+            }
         }
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/src/main/java/com/doranco/tp_expenses/ExpensesViewModel.kt
+++ app/src/main/java/com/doranco/tp_expenses/ExpensesViewModel.kt
@@ -1,16 +1,76 @@
 package com.doranco.tp_expenses
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 
 class ExpensesViewModel:ViewModel() {
 
-    val state = mutableStateOf(dummyExpenses)
+    val state = mutableStateOf(emptyList<Expense>())
+    private var restInterface: ExpensesApiService
 
-    val expenses = state.value.toMutableList()
-
-    fun totalPrice(price : Double) {
-        expenses
+    private val errorHandler = CoroutineExceptionHandler { _, exception ->
+        exception.printStackTrace()
     }
 
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .baseUrl(
+                "https://avid-racer-241421.firebaseio.com/"
+            )
+            .build()
+        restInterface = retrofit.create(
+            ExpensesApiService::class.java
+        )
+        getExpenses()
+    }
+
+    private suspend fun getRemoteExpenses() : List<Expense> {
+        return withContext(Dispatchers.IO){
+            restInterface.getExpenses()
+        }
+    }
+
+    private fun getExpenses() {
+        viewModelScope.launch(errorHandler) {
+            val expenses = getRemoteExpenses()
+            state.value = expenses
+        }
+    }
+
+    private suspend fun totalPrice(): Double {
+        var total: Double = 0.0
+
+        getRemoteExpenses().forEach {
+            total +=  it.price
+        }
+        return total
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private suspend fun selectDate(){
+        val dateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val today: LocalDate = LocalDate.now(ZoneId.of("Europe/Paris"))
+        val pastWeek: LocalDate = today.minusWeeks( 1 )
+
+        getRemoteExpenses().forEach {
+            var expenseDate = LocalDate.parse(it.date, dateTimeFormatter)
+            if (expenseDate.isAfter(pastWeek))
+                getExpenses()
+        }
+    }
 }
